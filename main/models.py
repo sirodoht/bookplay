872a84eeb7a7e2d85b9a294d92beaf3cb4552ca1
Theodore Keloglou <zf@sirodoht.com>
2023-03-11 18:14:59 +0000

    add models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Dialogue(models.Model):
+    slug = models.CharField(max_length=10)
+
+    class Meta:
+        ordering = ["-id"]
+
+    def __str__(self):
+        return self.slug
+
+
+class Response(models.Model):
+    dialogue = models.ForeignKey(Dialogue, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    # options: "system", "user", "assistant"
+    kind = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ["-id"]
+
+    def __str__(self):
+        return self.content[:30] + "..."
