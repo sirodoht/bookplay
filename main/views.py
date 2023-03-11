bc5fa31fadee75468859b9982ffbf2a0f79285bf
Theodore Keloglou <zf@sirodoht.com>
2023-03-11 18:15:21 +0000

    add chat page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main import chatgpt, models
+
+
+def index(request):
+    dialogue = models.Dialogue.objects.create(slug="war-and-peace-1")
+    models.Response.objects.create(
+        kind="system",
+        dialogue=dialogue,
+        content="You are a helpful assistant who helps people understand the novel War and Peace by Leo Tolstoy.",
+    )
+    return render(
+        request,
+        "main/index.html",
+        {
+            "response_list": models.Response.objects.filter(dialogue=dialogue).order_by(
+                "id"
+            )[1:],
+        },
+    )
+
+
+def chat(request):
+    dialogue = models.Dialogue.objects.all().order_by("-id").first()
+    chat_value = chatgpt.get_next_response(request.POST["talk"])
+    models.Response.objects.create(
+        kind="assistant",
+        dialogue=dialogue,
+        content=chat_value,
+    )
+    return render(
+        request,
+        "partials/chatresponse.html",
+        {
+            "message_list": [
+                {
+                    "content": request.POST["talk"],
+                    "kind": "user",
+                },
+                {
+                    "content": chat_value,
+                    "kind": "assistant",
+                },
+            ]
+        },
+    )
